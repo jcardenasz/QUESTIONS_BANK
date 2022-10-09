2e9dec6e2f7b65f36b39823759312cc35e38bed3
jcardenasz <80723420+jcardenasz@users.noreply.github.com>
2022-10-09 13:52:58 -0500

    Delete repeticion-espaciada directory

--- repeticion-espaciada/main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        //Descomente las lineas 4 y 5 para hacer una desmotración
-        Mazo mazo = new Mazo(Tarjeta.tarjetasDePrueba());
-        Interfaz.mostrarMenu(mazo);
-    }
-}
